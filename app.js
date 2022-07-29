fabf6e85645246b725c211cc3516e7e4f75c608b
Angel-Banks <angelonuchuks@gmail.com>
2022-07-29 23:49:32 +0100

    updated

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
+const https = require("https");
 
 
 
@@ -26,14 +27,62 @@ app.get("/", function(req, res) {
 
 
 app.post("/", function(req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
-  console.log(firstName, lastName, email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields : {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  }
 
+const jsonData = JSON.stringify (data);
+
+const url = "https://us14.api.mailchimp.com/3.0/lists/aa879c469d";
+const options = {
+  method : "POST",
+  auth : "angela1:2765ddcf437251ecab3a662139bdcff-us14"
+}
+const request = https.request (url, options, function (response){
+   if (response.statusCode === 200) {
+     res.sendFile(__dirname + "/success.html");
+
+   } else {
+     res.sendFile(__dirname + "/failure.html");
+
+   }
+
+ response.on ("data", function (data){
+   console.log(JSON.parse(data));
+ })
 });
+
+request.write (jsonData);
+request.end();
+});
+
+  app.post ("/failure", function (req, res){
+    res.redirect ("/");
+  })
 
 app.listen(3000, function() {
   console.log("Server is running on Port 3000.");
 })
+
+
+// API KEY
+
+// 22765ddcf437251ecab3a662139bdcff-us14
+
+
+// Audience or list or unque id
+//
+// aa879c469d
